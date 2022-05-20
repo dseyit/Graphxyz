1c0ee754b6a30accc085b32b9a96e3914632dd4f
Dovletgeldi Seyitliyev <seyitliyev@Dovletgeldis-MacBook-Pro.local>
2022-05-19 21:12:25 -0400

    just updating tests file

--- alltests.py
+++ alltests.py
@@ -1,3 +1,44 @@
+
+"""
+Created on Wed Nov 25 17:37:03 2020
+
+@author: d. seyitliyev
+"""
+#from funs_fit import *
+import numpy as np
+import sys
+import pathlib
+import platform
+import csv
+import pickle
+import ast
+from PyQt5.QtWidgets import QGraphicsOpacityEffect, QDesktopWidget, QWidget, QActionGroup, QMainWindow, QMenu, QMenuBar, QTableView, QMessageBox, QDialog, QApplication,QFileDialog, QPushButton, QSlider, QFrame, QLabel, QLineEdit, QCheckBox, QComboBox, QListWidget, QRadioButton, QTabWidget, QListView,QAbstractItemView,QTreeView, QColorDialog, QListWidgetItem
+from PyQt5 import QtWidgets
+import matplotlib
+import pandas as pd
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, uic
+from PyQt5.QtCore import QEasingCurve, pyqtSignal, QMimeData, QUrl, Qt, QAbstractTableModel, QPoint, QObject, QSettings, QTimer,QPropertyAnimation
+from PyQt5.QtGui import QFont, QColor, QScreen, QPixmap, QIcon 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+import matplotlib.backends.backend_svg
+from matplotlib.figure import Figure
+import matplotlib.cm as cm
+import os #Used in Testing Script
+import time
+import matplotlib.pyplot as plt
+import matplotlib.colors as clrs
+import matplotlib.legend as lgd
+from datetime import datetime
+from numpy import exp #Wil be used during user function input
+from numpy import log #Wil be used during user function input
+from numpy import cos #Wil be used during user function input
+from numpy import sin #Wil be used during user function input
+from numpy import tan #Wil be used during user function input
+from numpy import mod
+from numpy import linspace
+from scipy.optimize import curve_fit
+import math
 #This script is for test purposes only, does not serve any other purpose in this project
 
 ttorem = 'TAS'
@@ -10,3 +51,52 @@ del test[indtorem+1]
 test[0] = ttitles
 
 impw_list = test
+
+labelToShow='this is information'
+durationToShow = 4
+locationToShow = [500,500]
+
+start = time.time()
+testlabel = QLabel()
+testlabel.setStyleSheet("QLabel { background-color : white; color : green; }")
+testlabel.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+testlabel.setText(labelToShow)
+
+# eff = QGraphicsOpacityEffect()
+# testlabel.setGraphicsEffect(eff)
+# a = QPropertyAnimation(eff,b"opacity")
+# a.setDuration(durationToShow*1000*3)
+# a.setStartValue(0)
+# a.setEndValue(1)
+# a.setEasingCurve(QEasingCurve.InBack)
+# a.start(QPropertyAnimation.DeleteWhenStopped)
+
+eff = QGraphicsOpacityEffect()
+testlabel.setGraphicsEffect(eff)
+a = QPropertyAnimation(eff,b"opacity")
+a.setDuration(durationToShow*1000*3)
+a.setStartValue(1)
+a.setEndValue(0)
+a.setEasingCurve(QEasingCurve.OutBack)
+a.start(QPropertyAnimation.DeleteWhenStopped)
+
+testlabel.move(locationToShow[0],locationToShow[1])
+testlabel.show()
+testlabel.raise_()
+
+while time.time()-start<=durationToShow:
+    QApplication.processEvents()
+
+testlabel.close()
+
+# # in pyqt5 it needs to be PyQt5.QtWidgets
+# msg=QMessageBox() # create an instance of it
+# msg.setIcon(QMessageBox.Information) # set icon
+# msg.setText("This is a message box") # set text
+# msg.setInformativeText("This is additional information") # set information under the main text
+# msg.setWindowTitle("MessageBox demo") # set title
+# msg.setDetailedText("The details are as follows:") # a button for more details will add in
+# msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel) # type of buttons associated
+# #msg.buttonClicked.connect(myfunc) # connect clicked signal
+# return_value =msg.exec_() # get the return value
+# print("value of pressed message box button:", str(return_value)) # print result
